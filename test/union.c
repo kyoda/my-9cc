406230924681ae51370693c8a1f1f8979c19a816
Shigehisa KYODA <shigehisa.kyoda@gmail.com>
2023-09-18 12:32:59 +0900

    add union test file

---
+++ test/union.c
@@ -0,0 +1,11 @@
+#include "test.h"
+
+int main() {
+  ASSERT(4, ({ union {int a; char b[3];} y; sizeof(y);}));
+  ASSERT(8, ({ union t {int a; char b[5];} x; union t y; sizeof(y);}));
+  ASSERT(256, ({ union {int a; char b[2];} y; y.b[0] = 0; y.b[1] = 1; y.a;}));
+  ASSERT(1, ({ union {int a; char b[4];} y; y.a = 300; y.b[1];}));
+
+  printf("OK\n");
+  return 0;
+}
